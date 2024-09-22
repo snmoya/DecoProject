147e5941b05073b30c51d8fea9793e5a8de9bbda
Callm6Max <Max.t6@outlook.com>
2024-09-22 17:03:11 +1000

    Add files via upload

--- WebMLY/frontend/src/App.js
+++ WebMLY/frontend/src/App.js
@@ -3,11 +3,13 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
+import ZoneManagement from "./components/Zone Management/ZoneManagement";
 
 function App() {
   return (
     <div>
-      <Home />
+      {/*<Home />*/}
+        <ZoneManagement />
     </div>
   );
 }
