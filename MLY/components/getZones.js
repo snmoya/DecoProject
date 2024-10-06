c915bb170a978de7e276a06b70239bbaf6b06f62
Sebastian <moya.sebastian@hotmail.com>
2024-10-07 02:16:46 +1000

    Add getZones component that fetch zone from our API and update ShowMap to render those Zones

---
+++ MLY/components/getZones.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+
+const baseURL = 'https://deco3801-machineleads.uqcloud.net/api';
+const APIKEY = 'machine-leads-yesterday-secret';
+
+const useZones = () => {
+  const [zones, setZones] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchZones = async () => {
+      try {
+        const url = `${baseURL}/zones`;
+        const response = await fetch(url, {
+          headers: {
+            'Content-Type': 'application/json',
+            'x-api-key': APIKEY,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status}`);
+        }
+
+        const data = await response.json();
+        setZones(data); 
+        console.log("Fetched Zones: ", data); 
+      } catch (error) {
+        console.error('Error fetching zones:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchZones();
+  }, []);
+
+  return { zones, loading };
+};
+
+export default useZones;
