77bec7a56994706370633f8b9596e7a3a9601e47
Nhat Minh Le <lenhatminh451@gmail.com>
2024-10-08 03:36:33 +1000

    Separate Zone Popup Form into a component

--- WebMLY/frontend/src/pages/Map/Map.js
+++ WebMLY/frontend/src/pages/Map/Map.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 import { AuthContext } from '../../contexts/AuthContext';
 
 import CreateZoneButton from '../../components/CreateZoneButton/CreateZoneButton';
+import ZonePopupForm from '../../components/ZonePopupForm/ZonePopupForm';
 
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-draw/dist/leaflet.draw.css';
@@ -15,9 +16,8 @@ const Map = () => {
     const { orgId } = useContext(AuthContext);
 
     const [zones, setZones] = useState([]);
-    const [newZone, setNewZone] = useState(null);
     const [showPopup, setShowPopup] = useState(false);
-    const [zoneInfo, setZoneInfo] = useState({ name: '', address: '' });
+    const [zoneInfo, setZoneInfo] = useState({ name: '', address: '', polygon: null});
     const [drawnLayer, setDrawnLayer] = useState(null);     // Hold reference to the drawn zone
 
     // * Fetch zones from API
@@ -35,8 +35,7 @@ const Map = () => {
     // Reset form function to clean up the states
     const resetForm = () => {
         setShowPopup(false);
-        setZoneInfo({ name: '', address: '' });
-        setNewZone(null);
+        setZoneInfo({ name: '', address: '', polygon: null });
         setDrawnLayer(null);
     }
 
@@ -53,7 +52,8 @@ const Map = () => {
                 coordinates.push(coordinates[0]);  // Close the polygon
             }
 
-            setNewZone({
+            setZoneInfo({
+                ...zoneInfo,
                 polygon: coordinates
             });
 
@@ -64,60 +64,6 @@ const Map = () => {
             setShowPopup(true);
         }
     };
-
-    // * Handle form submission for the new zone
-    const handleZoneSubmit = async (e) => {
-        e.preventDefault();
-
-        if (!orgId) {
-            console.error('OrgId is missing!');
-            alert('Failed to create the zone because orgId is missing.');
-            return;
-        }
-
-        const zoneData = {
-            polygon: newZone.polygon,
-            name: zoneInfo.name,
-            address: zoneInfo.address,
-            org_id: orgId
-        };
-
-        console.log('Zone data:', zoneData);
-
-        try {
-            // Save the new zone to the backend
-            const response = await axios.post('/api/zones', zoneData);
-
-            // Extract the newly created zoneId from the backend response
-            const { zoneId } = response.data;
-
-            // Add the new zone with its ID to the list
-            setZones((prevZones) => [
-                ...prevZones,
-                { ...zoneData, id: zoneId } // Add the newly generated ID
-            ]);
-
-            // Associate the layer with the zoneId (used for deletion)
-            drawnLayer.options.zoneId = zoneId;
-            featureGroupRef.current.addLayer(drawnLayer);  // Add the layer to the FeatureGroup
-
-            // Reset the form and close the popup
-            resetForm();
-        } catch (error) {
-            console.error('Error creating zone:', error);
-            alert('Failed to create the zone.');
-        }
-    }
-
-    // * Handle cancel action (remove the drawn layer from the map)
-    const handleCancel = () => {
-        if (drawnLayer) {
-            featureGroupRef.current.removeLayer(drawnLayer);
-        }
-
-        // Reset the form and close the popup
-        resetForm();
-    }
 
     // * Handle zone deletion
     const handleDeleteZone = async (zoneId) => {
@@ -199,39 +145,14 @@ const Map = () => {
 
             {/* Popup form to enter new zone info */}
             {showPopup && (
-                <div>
-                    <form onSubmit={handleZoneSubmit} className="popup-form">
-                        <div className="popup-form-header">
-                            <h3>Create New Zone</h3>
-
-                            <button
-                                type="button"
-                                className="cancel-button"
-                                onClick={handleCancel}
-                            >
-                                &times;  {/* This will display the 'X' */}
-                            </button>
-                        </div>
-
-                        <label>Name:</label>
-                        <input
-                            type="text"
-                            value={zoneInfo.name}
-                            onChange={(e) => setZoneInfo({ ...zoneInfo, name: e.target.value })}
-                            required
-                        />
-
-                        <label>Address:</label>
-                        <input
-                            type="text"
-                            value={zoneInfo.address}
-                            onChange={(e) => setZoneInfo({ ...zoneInfo, address: e.target.value })}
-                            required
-                        />
-
-                        <button type="submit" className='save-button'>Save Zone</button>
-                    </form>
-                </div>
+                <ZonePopupForm
+                    setZones={setZones}
+                    zoneInfo={zoneInfo}
+                    setZoneInfo={setZoneInfo}
+                    drawnLayer={drawnLayer}
+                    featureGroupRef={featureGroupRef}
+                    resetForm={resetForm}
+                />
             )}
         </div>
     )
