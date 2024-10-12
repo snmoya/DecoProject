ab6ad54a737de5b078cb352987e5b300591e6e41
Nhat Minh Le <lenhatminh451@gmail.com>
2024-10-13 05:39:31 +1000

    Implement Edit Zone Info logic

--- website-frontend/src/components/ZoneList/ZoneList.js
+++ website-frontend/src/components/ZoneList/ZoneList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './ZoneList.css';
 
-const ZoneList = ({ zones, setShowZoneList }) => {
+const ZoneList = ({ zones, setShowZoneList, setShowZoneInfoModal, setSelectedZone }) => {
     return (
         <div className="zone-list-popup">
             <span 
@@ -19,7 +19,15 @@ const ZoneList = ({ zones, setShowZoneList }) => {
                     <p>No zones available</p>
                 ) : (
                     zones.map((zone) => (
-                        <li key={zone.id}>
+                        <li 
+                            key={zone.id} 
+                            className="zone-item" 
+                            title="Click to edit zone info"
+                            onClick={() => {
+                                setSelectedZone(zone);
+                                setShowZoneInfoModal(true);
+                            }}
+                        >
                             {zone.name}
                         </li>
                     ))
