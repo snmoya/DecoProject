31991abd8c19b463c98c6ac23e467ec380773fca
Nhat Minh Le <lenhatminh451@gmail.com>
2024-10-17 05:07:55 +1000

    Fix Protected Route with new Loading State

--- website-frontend/src/contexts/ProtectedRoute.js
+++ website-frontend/src/contexts/ProtectedRoute.js
@@ -2,8 +2,14 @@ import React, { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import { AuthContext } from '../contexts/AuthContext';
 
+import LoadingState from '../components/LoadingState/LoadingState';
+
 const ProtectedRoute = ({ children }) => {
-    const { isAuthenticated } = useContext(AuthContext);
+    const { isAuthenticated, loading } = useContext(AuthContext);
+
+    if (loading) {
+        return <LoadingState />;
+    }
 
     return isAuthenticated ? children : <Navigate to="/login" />;
 };
